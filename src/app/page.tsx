a35e0ffb61130038ec2d5130790c333956d8738d
sanjeevshah1 <sahsanjeev42@gmail.com>
2025-01-24 21:13:33 +0545

    new path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "./../components/Home"
-import About from "./../components/About";
+import HomePage from "./components/Home"
+import About from "./components/About";
 export default function Home() {
   return (
     <>
