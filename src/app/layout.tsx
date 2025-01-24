2db8f67596c8e23fef29c7b17f7e557c754f4fb5
sanjeevshah1 <sahsanjeev42@gmail.com>
2025-01-24 21:02:26 +0545

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/Components/Header";
+import Header from "@/components/Header";
 import { Josefin_Slab } from "next/font/google";
 
 const josefinSlab = Josefin_Slab({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "@/Components/Home"
-import About from "@/Components/About";
+import HomePage from "@/components/Home"
+import About from "@/components/About";
 export default function Home() {
   return (
     <>
